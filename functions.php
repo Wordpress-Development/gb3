b6b9ff890f5a5a1ca32b3c70cf1aa8ab3fd59751
Bryan Willis <willis.bryan@live.com>
2015-11-20 13:03:17 -0600

    Add Theme Stylesheet Override

--- functions.php
+++ functions.php
@@ -1,7 +1,22 @@
 <?php
-//* Start the engine
-include_once( get_template_directory() . '/lib/init.php' );
+//* Start the engine 
+include_once( get_template_directory() . '/lib/init.php' ); // Do Not Edit This Line
 
+
+
+
+/*-----------------------------------------------------------------------------------*/
+/* BEGIN CUSTOM THEME FUNCTIONS BELOW */
+/*-----------------------------------------------------------------------------------*/
+
+
+//*
+// Override Bootstrap Stylesheet - http://bootstrap-live-customizer.com/
+function gb3_child_css_override( $css_url ) {
+	return CHILD_CSS . 'theme.min.css';
+}
+add_filter( 'bsg_pre_load_css', 'gb3_child_css_override' );
+// */
 
 
 
